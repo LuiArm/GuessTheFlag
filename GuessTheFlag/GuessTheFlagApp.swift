9b43bdda84c1e02c68e4ec73eafe09bb1b2ed6af
LuiArm <Luisarmendariz24@icloud.com>
2023-03-28 15:19:16 -0600

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by luis armendariz on 3/28/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
