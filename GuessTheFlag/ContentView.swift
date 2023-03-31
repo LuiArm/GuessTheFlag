fa51975e87f76e0ddae6df933ae606e81ad1182f
LuiArm <Luisarmendariz24@icloud.com>
2023-03-30 21:49:20 -0600

    Added button and alert functionality

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -13,31 +13,40 @@ struct ContentView: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     
     @State private var correctAnwer = Int.random(in: 0...2)
+    @State private var score: Int = 0
     
     var body: some View {
         ZStack {
-            Color.blue
+            LinearGradient(gradient: Gradient(colors: [.blue,.black]), startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
             
             VStack(spacing: 30){
                 VStack {
                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                        .font(.subheadline.weight(.heavy))
                     Text(countries[correctAnwer])
+                        .foregroundColor(.white)
+                        .font(.largeTitle.weight(.semibold))
                 }
                 
                 ForEach(0..<3){ number in
                     Button {
-                        flagTapped(number)                    } label: {
+                        flagTapped(number)
+                    } label: {
                         Image(countries[number])
                             .renderingMode(.original)
+                            .clipShape(Capsule())
+                            .shadow(radius: 5)
                     }
+                    
                 }
             }
         }
         .alert(scoreTitle, isPresented: $showingScore){
             Button("Continue", action: askQuestion)
         } message: {
-            Text("Your score is ???")
+            Text("Your score is \(score)")
         }
     }
     
@@ -50,8 +59,17 @@ struct ContentView: View {
         showingScore = true
     }
     
+//    func checkScore(_ number: Int) -> Int{
+//        if number == correctAnwer{
+//            $score+=1
+//        }else{
+//            $score-=1
+//        }
+//        return score
+//    }
+//
     func askQuestion() {
-        countries.shuffled()
+        countries.shuffle()
         correctAnwer = Int.random(in: 0...2)
     }
 }
